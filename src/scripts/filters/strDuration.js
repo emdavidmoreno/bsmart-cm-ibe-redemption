39436f831aed6960d73ba6b3d11fe25908ecb8f6
Yolier Galan Tasse <gallegogt@gmail.com>
2017-02-26 20:24:33 -0300

    Fix hour format

--- src/scripts/filters/strDuration.js
+++ src/scripts/filters/strDuration.js
@@ -1,29 +1,31 @@
 /* jshint browser:true */
 /* jshint -W003*/
-'use strict';
+'use strict'
 define([], function() {
   /**
    * Angular filter to convert number to time string
    * e.g 120 => 2h 00m
+   * @return {string}
    */
   function strDuration() {
     return function(input) {
-      var duration = parseInt(input, 10);
-      var hours;
+      let duration = parseInt(input, 10)
+      let hours
       if(!isNaN(duration) || duration > 0) {
         // get hours from duration
-        hours = Math.floor(duration / 60);
+        hours = Math.floor(duration / 60)
         // get mins
-        var mins = Math.floor(duration % 60);
-        // return format '00h 00m'
-        return `${hours}h ${mins}m`;
-      } else {
-        return '00h 00m';
+        let mins = Math.floor(duration % 60)
+        // 00m
+        mins = mins > 10 ? mins : `0${mins}`
+        // format '00h 00m'
+        return `${hours}h ${mins}m`
       }
-    };
+      return '00h 00m'
+    }
   }
 
-  strDuration.$inject = [];
+  strDuration.$inject = []
 
-  return strDuration;
-});
+  return strDuration
+})
