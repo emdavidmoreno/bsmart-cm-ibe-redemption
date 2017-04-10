e45ad1fc96cffa05bfae1596ed2a456a83bcd104
Yolier Galan Tasse <gallegogt@gmail.com>
2017-04-09 23:09:55 -0300

    Fixed bs flexible-calendar using farenet2

--- src/components/complex/bs-flexible-dates-prices/helpers/scrapHelper.js
+++ src/components/complex/bs-flexible-dates-prices/helpers/scrapHelper.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($jq) {
   'use strict'
   // selectors
-  let SELECTOR_CELLS = '.lowest'
+  let SELECTOR_CELLS = 'td[onclick],.notAvail'
 
   return {
     getCells: function() {
@@ -11,27 +11,29 @@ define(['jquery'], function($jq) {
       }
       $(SELECTOR_CELLS).each((index, el) => {
         let $el = $(el)
-        let [outboundDate, inboundDate] =
-          $el.attr('onclick').match(/.*\'(.*)\'.*\'(.*)\'/).slice(1)
+        if(!$el.is('.notAvail')) {
+          let [outboundDate, inboundDate] =
+            $el.attr('onclick').match(/.*\'(.*)\'.*\'(.*)\'/).slice(1)
 
-        let price = $el.has('div.colPrice').text().trim()
-        let isSelected = $el.is('.selected')
-        let msOutboundDate = Date.parse(outboundDate)
+          let price = $el.has('div.colPrice').text().trim()
+          let isSelected = $el.is('.selected')
+          let msOutboundDate = Date.parse(outboundDate)
 
-        if(!cells.outboundDates.hasOwnProperty(msOutboundDate)) {
-          cells.outboundDates[msOutboundDate] = []
+          if(!cells.outboundDates.hasOwnProperty(msOutboundDate)) {
+            cells.outboundDates[msOutboundDate] = []
+          }
+          cells.inboundDates[Date.parse(inboundDate)] = {}
+
+          cells.outboundDates[msOutboundDate].push({
+            inboundDate,
+            price,
+            isSelected,
+            jqElement: $el,
+            onClick: function() {
+              this.jqElement[0].click()
+            },
+          })
         }
-        cells.inboundDates[Date.parse(inboundDate)] = {}
-
-        cells.outboundDates[msOutboundDate].push({
-          inboundDate,
-          price,
-          isSelected,
-          jqElement: $el,
-          onClick: function() {
-            this.jqElement[0].click()
-          },
-        })
       })
       return cells
     },
