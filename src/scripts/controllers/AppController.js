df6623013763d940d947dae793f96d638f3d27f0
erlin1989 <erlin.multisystem@gmail.com>
2017-06-28 16:32:10 -0400

    added Object.assign if not defined

--- src/scripts/controllers/AppController.js
+++ src/scripts/controllers/AppController.js
@@ -66,7 +66,7 @@ define([
         console.log('AppController init');
         hostUIService.bindUI();
         hostUIService.scrollToTop();
-        hostUIService.hideCopaSideAd(); 
+        hostUIService.hideCopaSideAd();
       };
 
       // This solution is temporal, the best choice is set Babel to support Object entries method and Object values method.
@@ -82,6 +82,38 @@ define([
             y.push(x[z]) && y, []);
       }
 
+      if (!Object.assign) {
+        Object.defineProperty(Object, 'assign', {
+          enumerable: false,
+          configurable: true,
+          writable: true,
+          value: function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+              throw new TypeError('Cannot convert first argument to object');
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+              var nextSource = arguments[i];
+              if (nextSource === undefined || nextSource === null) {
+                continue;
+              }
+              nextSource = Object(nextSource);
+
+              var keysArray = Object.keys(nextSource);
+              for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                var nextKey = keysArray[nextIndex];
+                var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                if (desc !== undefined && desc.enumerable) {
+                  to[nextKey] = nextSource[nextKey];
+                }
+              }
+            }
+            return to;
+          }
+        });
+      }
 
       //-------------------------------------------------------
       // binding properties
