c023a338ab0e218f1ada55e1f712cde8ad5d00b1
David Moreno <devel.enterprise@gmail.com>
2018-05-08 18:45:35 -0400

    refactor jqui-dialog

--- src/scripts/directives/jqui-dialog.js
+++ src/scripts/directives/jqui-dialog.js
@@ -9,15 +9,12 @@ define([
    * Angular directive that create a wrapper for jQueryUI dialog
    */
    function jquiDialog ($timeout) {
-    var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
-    var firstTabStop = null, lastTabStop = null, focusableElements = []
      return {
        restrict: 'A',
        scope: {
          bsOpenDialog: '@',
          bsCloseFromInner: '=',
          bsLastActive: "@"
-
        },
        replace:false,
        transclude: true,  // transclusion allows for the dialog
@@ -25,15 +22,13 @@ define([
        template: '<div ng-transclude></div>',
         link: function($scope, element, attrs) {
           var $element = $(element);
-          
           var dialogOptions = {
             autoOpen: false,
             modal: true,
-            closeOnEscape: false,
             width: attrs.width || 350,
             height: attrs.height || 200,
             draggable: false,
-            resizable: false,           
+            resizable: false,
             open: function() {
               $('.ui-widget-overlay').bind('click', function() {
                 $timeout(function() {
@@ -43,7 +38,7 @@ define([
               });
 
               // Find all focusable children              
-              focusableElements = $(element).find(focusableElementsString);
+              focusableElements = $element.find(focusableElementsString);
               // Convert NodeList to Array
               focusableElements = Array.prototype.slice.call(focusableElements);
 
@@ -59,30 +54,38 @@ define([
                 return;                             
               var lastAtive = document.getElementById($scope.bsLastActive)
               lastAtive.focus()
-              document.removeEventListener("keydown", trapTabKey, true)
+              //document.removeEventListener("keydown", trapTabKey, true)
             }
           };
           // This works when observing an interpolated attribute
           // e.g {{dialogOpen}}.  In this case the val is always a string and so
           // must be compared with the string 'true' and not a boolean
           // using open: '@' and open="{{dialogOpen}}"
-          attrs.$observe('bsOpenDialog', function(val) {            
+          attrs.$observe('bsOpenDialog', function(val) {
             if (val === 'true') {
               $element.dialog('open');
             }
             else {
-              $element.dialog('close');                 
+              $element.dialog('close');
             }
           });
 
-          $element.dialog(dialogOptions)
+          $element.dialog(dialogOptions);
 
+          //Auxiliary code
+          var focusableElementsString = `a[href], area[href], input:not([disabled]), 
+          select:not([disabled]), textarea:not([disabled]), button:not([disabled]), 
+          iframe, object, embed, [tabindex="0"], [contenteditable] `;
+          var firstTabStop = null, 
+              lastTabStop = null, 
+              focusableElements = []
+          
           let trapTabKey = evt => {
               //escape key
             if(evt.keyCode == 27){
               $element.find(".close").click()
             }
-             // Check for TAB key press
+              // Check for TAB key press
             if (evt.keyCode === 9) {
               // SHIFT + TAB
               if (evt.shiftKey) {
@@ -100,7 +103,6 @@ define([
             }
             evt.stopPropagation();
           }
-
           document.addEventListener("keydown", trapTabKey, true)
         }
      };
@@ -109,4 +111,5 @@ define([
   jquiDialog.$inject = ['$timeout'];
 
   return jquiDialog;
+  
 });
